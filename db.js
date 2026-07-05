16224c63ae9ac9dcc1a96225faec4d345ce91a12
𝚆𝚘𝚠. 𝙲𝚘𝚖 <160624200+Muhamd-ilham@users.noreply.github.com>
2026-07-06 03:42:14 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,15 +1,17 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    connectionString: 'postgresql://postgres:12345@localhost:5432/bos_fifa_db'
+    // 👇 Ini sudah diganti pakai jalur satelit ke Neon.tech
+    connectionString: 'postgresql://neondb_owner:npg_T74cdWsOunVR@ep-floral-leaf-atar110d-pooler.c-9.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
 });
 
+// Penangkal error kalau database Neon lagi "tidur"
 pool.on('error', (err) => {
-    console.error('Koneksi database terputus:', err.message);
+    console.error('Koneksi database terputus atau Neon sedang tidur:', err.message);
 });
 
 pool.connect()
-    .then(() => console.log('Sukses terhubung ke PostgreSQL LOKAL!'))
+    .then(() => console.log('🔥 Sukses terhubung ke PostgreSQL (Neon Cloud)!'))
     .catch(err => console.error('Gagal koneksi ke database:', err.stack));
 
 module.exports = pool;
